dbf5120062f520b20613e04e630897ea18f6a0c7
Murat Iyigun <miyigun@hotmail.com>
2024-01-13 21:39:22 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:realtime_object_detection/views/camera_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RealTime Object Detection',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const CameraView(),
+    );
+  }
+}
+
